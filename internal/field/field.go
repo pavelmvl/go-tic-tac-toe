50e6aa7e53ed12be147b7af84f84b951a79b5b47
pavelmvl <pavelmvl@gmail.com>
2022-02-14 13:42:17 +0300

    add methods IsFieldFull(), ToString(), Print() to package field, struct Field

--- internal/field/field.go
+++ internal/field/field.go
@@ -47,3 +47,35 @@ func (f *Field) AssignCell(col, row int, mark rune) error {
 func (f Field) GetCellValue(col, row int) rune {
 	return f.cells[col][row]
 }
+
+func (f Field) IsFieldFull() bool {
+	for col := 0; col < f.size; col++ {
+		for row := 0; row < f.size; row++ {
+			if f.GetCellValue(col, row) == NoWinner {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (f Field) ToString() string {
+	buf := make([]byte, 0, f.size*(2*f.size+1)+1)
+	for row := 0; row < f.size; row++ {
+		buf = append(buf, []byte(" ")...)
+		for col := 0; col < f.size; col++ {
+			mark := f.GetCellValue(col, row)
+			if mark == NoWinner {
+				mark = ' '
+			}
+			buf = append(buf, []byte(string(mark))...)
+			buf = append(buf, []byte(" ")...)
+		}
+		buf = append(buf, []byte("\n")...)
+	}
+	return string(buf)
+}
+
+func (f Field) Print() {
+	print(f.ToString())
+}
