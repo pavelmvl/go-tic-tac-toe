6bf8dd31d9da303bafc42bbddfe2764150e7f319
pavelmvl <pavelmvl@gmail.com>
2022-02-20 22:54:09 +0300

    add func startBrowser(), copied from tour of golang

--- cmd/tic-tac-toe/main.go
+++ cmd/tic-tac-toe/main.go
@@ -63,3 +63,21 @@ func main() {
 		iter++
 	}
 }
+
+/// NOTE This func is copied from tour of golang
+// startBrowser tries to open the URL in a browser, and returns
+// whether it succeed.
+func startBrowser(url string) bool {
+	// try to start the browser
+	var args []string
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{"open"}
+	case "windows":
+		args = []string{"cmd", "/c", "start"}
+	default:
+		args = []string{"xdg-open"}
+	}
+	cmd := exec.Command(args[0], append(args[1:], url)...)
+	return cmd.Start() == nil
+}
