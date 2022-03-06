bf8291d4d1417039c7dfb2c58bbd4df0fcc30bdb
pavelmvl <pavelmvl@gmail.com>
2022-03-06 17:04:06 +0300

    update unit tests of package config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -3,41 +3,105 @@ package config
 import (
 	"bytes"
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 )
 
-var conf = []byte(`{
-  "game_variant":"http",
-  "players_sides":"OX",
-  "field_size":3,
-  "field_winseq":3
-}`)
-
-var confMerge = []byte(`{
-  "game_variant":"http",
-  "players_sides":"OX",
-  "field_size":3,
-}`)
-
-func TestJsonMerge(t *testing.T) {
-	Buffer := bytes.NewBuffer(conf)
-	c := NewEmptyConfig()
-	c.FieldSize = 4
-	c.FieldWinSeq = 4
-	fmt.Println(c)
-	c.ReadJsonAndMerge(Buffer)
-	fmt.Println(c)
-}
-
-func TestReadJsonAndMerge(t *testing.T) {
-	Buffer := bytes.NewBuffer(conf)
-	c := NewEmptyConfig()
-	c.ReadJsonAndMerge(Buffer)
-	fmt.Println(c)
+func TestMain(t *testing.M) {
+	// Store environment
+	env := os.Environ()
+	os.Clearenv()
+	defer func() {
+		os.Clearenv()
+		for _, e := range env {
+			idx := strings.Index(e, "=")
+			key := e[:idx]
+			value := e[idx+1:]
+			err := os.Setenv(key, value)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}()
+	code := t.Run()
+	os.Exit(code)
 }
 
 func TestReadEnvAndMerge(t *testing.T) {
-	c := NewEmptyConfig()
+	defer os.Clearenv()
+	var c, cmp Config
+	// first: full env read test
+	os.Clearenv()
+	c = NewEmptyConfig()
+	os.Setenv("TICTACTOE_GAME_VARIANT", "http")
+	os.Setenv("TICTACTOE_PLAYERS_SIDES", "XO")
+	os.Setenv("TICTACTOE_FIELD_SIZE", "3")
+	os.Setenv("TICTACTOE_FIELD_WINSEQ", "3")
 	c.ReadEnvAndMerge()
-	fmt.Println(c)
+	cmp = Config{
+		GameVariant:  "http",
+		PlayersSides: "XO",
+		FieldSize:    3,
+		FieldWinSeq:  3,
+	}
+	if c != cmp {
+		t.Fatal("full env read test FAIL")
+	}
+	// second: merge env test
+	os.Clearenv()
+	c = NewEmptyConfig()
+	c.PlayersSides = "XO"
+	os.Setenv("TICTACTOE_GAME_VARIANT", "cli")
+	os.Setenv("TICTACTOE_FIELD_SIZE", "4")
+	os.Setenv("TICTACTOE_FIELD_WINSEQ", "4")
+	c.ReadEnvAndMerge()
+	cmp = Config{
+		GameVariant:  "cli",
+		PlayersSides: "XO",
+		FieldSize:    4,
+		FieldWinSeq:  4,
+	}
+	if c != cmp {
+		t.Fatal("merge env test FAIL")
+	}
+}
+
+var conf = []byte(
+	`{"game_variant":"http","players_sides":"OX","field_size":3,"field_winseq":3}`,
+)
+var confMerge = []byte(
+	`{"game_variant":"http","players_sides":"OX","field_size":4}`,
+)
+
+func TestJsonMerge(t *testing.T) {
+	var Buffer *bytes.Buffer
+	var c, cmp Config
+	// first: read full json config
+	Buffer = bytes.NewBuffer(conf)
+	c = NewEmptyConfig()
+	c.ReadJsonAndMerge(Buffer)
+	cmp = Config{
+		GameVariant:  "http",
+		PlayersSides: "OX",
+		FieldSize:    3,
+		FieldWinSeq:  3,
+	}
+	if c != cmp {
+		t.Fatal("full json config read FAIL")
+	}
+	// second: merge json config
+	Buffer = bytes.NewBuffer(confMerge)
+	c = NewEmptyConfig()
+	c.FieldWinSeq = 3
+	c.ReadJsonAndMerge(Buffer)
+	cmp = Config{
+		GameVariant:  "http",
+		PlayersSides: "OX",
+		FieldSize:    4,
+		FieldWinSeq:  3,
+	}
+	if c != cmp {
+		t.Fatal("merge json config read FAIL")
+	}
 }
