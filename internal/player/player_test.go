ab69a206a522f7dab029b03ab9815b57208ff88c
pavelmvl <pavelmvl@gmail.com>
2022-03-12 20:16:40 +0300

    fix tests in package internal/player

--- internal/player/player_test.go
+++ internal/player/player_test.go
@@ -8,19 +8,19 @@ func TestPlayer(t *testing.T) {
 	// t.Fatal("not implemented")
 	var player Player
 	player = New("X")
-	if player.Priority != 0 || player.Mark != 'X' {
+	if player.Priority != 0 || player.GetMark() != 'X' {
 		t.Fatal("do not pass cmd -p X")
 	}
 	player = New("x")
-	if player.Priority != 0 || player.Mark != 'X' {
+	if player.Priority != 0 || player.GetMark() != 'X' {
 		t.Fatal("do not pass cmd -p x")
 	}
 	player = New("O")
-	if player.Priority != 1 || player.Mark != 'O' {
+	if player.Priority != 1 || player.GetMark() != 'O' {
 		t.Fatal("do not pass cmd -p o")
 	}
 	player = New("")
-	if player.Priority != 1 || player.Mark != 'O' {
+	if player.Priority != 1 || player.GetMark() != 'O' {
 		t.Fatal("do not pass cmd -p P")
 	}
 }
